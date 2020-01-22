4569e1a7a003714a3841a81ab5b22573a9251a1c
sshpakkk <forrestmax63@gmail.com>
2020-01-22 03:01:04 +0300

    22.01

--- camp/src/app/buy/buy.component.ts
+++ camp/src/app/buy/buy.component.ts
@@ -11,9 +11,9 @@ export class BuyComponent implements OnInit {
   constructor() { }
   table: Array<ReservationItem> = [
     { id: 0, part: "1-ая(июнь)", theme: "игры с мячом", squad: "баскетбол, футбол, большой теннис,гандбол, крикет", AllPlaces: 150, CountOfVacancies: 143 },
-    { id: 1, part: "1-ая(июнь)", theme: "игры с мячом", squad: "баскетбол, футбол, большой теннис,гандбол, крикет", AllPlaces: 150, CountOfVacancies: 143 },
-    { id: 2, part: "1-ая(июнь)", theme: "игры с мячом", squad: "баскетбол, футбол, большой теннис,гандбол, крикет", AllPlaces: 150, CountOfVacancies: 143 },
-    { id: 3, part: "1-ая(июнь)", theme: "игры с мячом", squad: "баскетбол, футбол, большой теннис,гандбол, крикет", AllPlaces: 150, CountOfVacancies: 143 },
+    { id: 1, part: "2-ая(июль)", theme: "летние виды спорта", squad: "плавание, лёгкая атлетика, бег, спортивное ориентирование", AllPlaces: 125, CountOfVacancies: 119 },
+    { id: 2, part: "3-яя(август)", theme: "единоборства", squad: "греко-римская борьба, бокс, кик-боксинг, армейский рукопашный бой, военно-тактические игры( страйкбол, пейнтбол )", AllPlaces: 125, CountOfVacancies: 88 },
+    { id: 3, part: "4-ая(февраль)", theme: "зимние виды спорта", squad: "кёрлинг, хокей, горные лыжи, сноубординг, фигурное катание, саллом", AllPlaces: 95, CountOfVacancies: 41 },
   ];
   toBook(id: number) {
     console.log(id);
